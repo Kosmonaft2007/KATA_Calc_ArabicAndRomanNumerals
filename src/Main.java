bdbbe62ce5c887c17084f5ce88896e5bb860b7e6
Vadim <kosmonaft2007@ya.ru>
2023-03-12 08:39:17 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+
+        double a = 125.4f;
+        double r = a/Math.PI;
+        System.out.printf("R = %.3f \n", a/Math.PI);
+
+
     }
 }
