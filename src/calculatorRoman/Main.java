c10c4337ef1abc5ff2e873d6bd5ccc482d6604c0
Vadim <kosmonaft2007@ya.ru>
2023-03-29 21:33:05 +0300

    в римской системе

--- src/calculatorRoman/Main.java
+++ src/calculatorRoman/Main.java
@@ -2,43 +2,6 @@ package calculatorRoman;
 
 import Calc_AR.ConverterCalc;
 
-/*
-
-
-- Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более. На выходе числа не ограничиваются по величине и могут быть любыми.
-
-- При вводе пользователем строки, не соответствующей одной из вышеописанных арифметических операций, приложение выбрасывает исключение и завершает свою работу.
-
-
-Результатом работы калькулятора с римскими числами могут быть только положительные числа, если результат работы меньше единицы, выбрасывается исключение
-
-
-Input:
-I - II
-
-Output:
-throws Exception //т.к. в римской системе нет отрицательных чисел
-
-Input:
-I + 1
-
-Output:
-throws Exception //т.к. используются одновременно разные системы счисления
-
-Input:
-1
-
-Output:
-throws Exception //т.к. строка не является математической операцией
-
-Input:
-1 + 2 + 3
-
-Output:
-throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)
-
-
-*/
 public class Main {
     public static void main(String[] args) throws RuntimeException {
         // Ввод текста с терминала
@@ -79,9 +42,17 @@ public class Main {
         int result = switch (action) {
             case "+" -> a + b;
             case "-" -> a - b;
+
             case "/" -> (int) a / b;
             default -> a * b;
         };
+
+//        if (isRoman)
+//        {
+//            converter.intToRoman(result) <= 0;
+//            throw new RuntimeException("в римской системе нет отрицательных чисел");
+//        }
+
 
         System.out.println(isRoman ? converter.intToRoman(result) : result);
     }
